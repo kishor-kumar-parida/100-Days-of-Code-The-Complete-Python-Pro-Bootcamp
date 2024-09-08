ed8ef71979d33fe417c16edff0c19ee76e36bfa8
Kishor kumar parida <126803569+kishor-kumar-parida@users.noreply.github.com>
2024-09-08 18:40:08 +0530

    Add files via upload

---
+++ DAY - 6/hurdle1.py
@@ -0,0 +1,20 @@
+# Hurdle Link - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+
+# Solution -
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# for i in range(6):
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
