55899cc8a1a90a09f719e4c6390c9940ae7886a2
Kishor kumar parida <126803569+kishor-kumar-parida@users.noreply.github.com>
2024-09-18 20:04:55 +0530

    Add files via upload

---
+++ DAY - 16/practice.py
@@ -0,0 +1,29 @@
+# import turtle
+# # from turtle import Turtle
+
+
+# timmy = turtle.Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+
+
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# ================================================ #
+
+# # pip install prettytable
+# from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column("name", ["kishor", "kumar", "parida"])
+# table.add_column("age", [10, 20, 30])
+
+# table.align = "c"
+# print(table)
+
+# ================================================ #
+
