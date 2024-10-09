6526b0a2a7b5392d4860ea519bd5b3daa49cb9b8
Kishor kumar parida <126803569+kishor-kumar-parida@users.noreply.github.com>
2024-10-09 10:46:13 +0530

    Add files via upload

---
+++ DAY - 37/main.py
@@ -0,0 +1,76 @@
+import requests
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "XEUNGR45A78OS5N1"
+NEWS_API_KEY = "b3715c881c414f0b84f4eed62a70b27c"
+
+
+## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+# Get yesterday's closing stock price
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+# print(yesterday_closing_price)
+
+
+# Get the day before yesterday's closing stock price
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+# print(day_before_yesterday_closing_price)
+
+
+# Find the positive difference between 1 and 2.
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+# Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+# print(diff_percent)
+
+
+## STEP 2: Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+# Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+# If difference percentage is greater than 5 then print("Get News").
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [
+        f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}"
+        for article in three_articles
+    ]
+
+    for article in formatted_articles:
+        print(article)
